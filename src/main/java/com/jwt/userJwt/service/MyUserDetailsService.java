ae658b9f5f0a1e905478119551b8e27423581594
VegiSujini <sujini.vegi@gmail.com>
2024-07-24 11:33:48 +0530

    modified MyuserDetailsService file

--- src/main/java/com/jwt/userJwt/service/MyUserDetailsService.java
+++ src/main/java/com/jwt/userJwt/service/MyUserDetailsService.java
@@ -2,8 +2,6 @@ package com.jwt.userJwt.service;
 
 import java.util.Optional;
 
-import javax.jws.soap.SOAPBinding.Use;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,13 +12,13 @@ import com.jwt.userJwt.entity.User;
 import com.jwt.userJwt.repository.UserRepository;
 
 @Service
-public class MyUserDetailsService implements UserDetailsService{
+public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
-    
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(username);
-        user.orElseThrow(()->new UsernameNotFoundException("Not found : "+username));
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found : " + username));
         return user.map(MyUserDetails::new).get();
     }
 }
