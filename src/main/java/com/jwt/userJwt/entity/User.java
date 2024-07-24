0075ba880ffc499e78c8d119cfbf41832a5272a8
VegiSujini <sujini.vegi@gmail.com>
2024-07-24 10:43:24 +0530

    added user file

---
+++ src/main/java/com/jwt/userJwt/entity/User.java
@@ -0,0 +1,25 @@
+package com.jwt.userJwt.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String userName;
+    private String password;
+    private Set<String> roles;
+}
