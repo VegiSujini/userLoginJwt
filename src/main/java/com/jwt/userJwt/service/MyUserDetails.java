089c543f6e6774af9d58b4bc2dc738293ff9327d
VegiSujini <sujini.vegi@gmail.com>
2024-07-24 11:32:36 +0530

    added MyUserDetails file

---
+++ src/main/java/com/jwt/userJwt/service/MyUserDetails.java
@@ -0,0 +1,53 @@
+package com.jwt.userJwt.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.jwt.userJwt.entity.User;
+
+public class MyUserDetails implements UserDetails {
+    private User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public List<GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
