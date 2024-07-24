ba36277beb82c3cf9bb60482d7dc3ccdf1d07bd9
VegiSujini <sujini.vegi@gmail.com>
2024-07-24 10:37:28 +0530

    first commit

---
+++ src/test/java/com/jwt/userJwt/UserJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.userJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
